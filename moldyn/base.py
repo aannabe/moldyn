67f7638fef387cc0159d71a52a42a26e202a8815
gani <annaberdiyev@gmail.com>
2024-01-08 21:35:01 -0500

    add docstrings

--- moldyn/base.py
+++ moldyn/base.py
@@ -3,6 +3,36 @@
 import numpy as np
 
 class Particle:
+    """Class representing a particle in a simulation.
+
+    Parameters
+    ----------
+    location : list of float, optional
+        Initial position of the particle. Default is [None, None].
+    velocity : list of float, optional
+        Initial velocity of the particle. Default is [None, None].
+    acceleration : list of float, optional
+        Initial acceleration of the particle. Default is [None, None].
+
+    Attributes
+    ----------
+    _MASS : float
+        Mass of the particle.
+    location : numpy.ndarray
+        Current position of the particle.
+    velocity : numpy.ndarray
+        Current velocity of the particle.
+    acceleration : numpy.ndarray
+        Current acceleration of the particle.
+
+    Methods
+    -------
+    get_kinetic_energy()
+        Calculate the kinetic energy of the particle.
+    get_momentum()
+        Calculate the momentum of the particle.
+    """
+
     def __init__(self, location = [None, None], velocity = [None, None], acceleration = [None, None]):
         self._MASS = 1.0
         self.location = np.array(location)
@@ -11,11 +41,25 @@ class Particle:
     #end __init__
 
     def get_kinetic_energy(self):
+        """Calculate the kinetic energy of the particle.
+
+        Returns
+        -------
+        float
+            The kinetic energy of the particle.
+        """
         kinetic_energy = self._MASS * (self.velocity**2).sum() * 0.5 # KE = (mv^2)/2
         return kinetic_energy
     # end get_kinetic_energy
 
     def get_momentum(self):
+        """Calculate the momentum of the particle.
+
+        Returns
+        -------
+        numpy.ndarray
+            The momentum of the particle.
+        """
         momentum = self._MASS * self.velocity # M = mv
         return momentum
     # end get_momentum
@@ -23,11 +67,43 @@ class Particle:
 # end Particle
 
 class Interaction:
+    """Class representing particle interactions.
+
+    Parameters
+    ----------
+    R_CUT : float
+        Cutoff radius for interactions.
+
+    Attributes
+    ----------
+    __R_CUT : float
+        Cutoff radius for interactions.
+
+    Methods
+    -------
+    LJ_potential(r)
+        Calculate the Lennard-Jones potential for a given distance.
+    LJ_force(r)
+        Calculate the Lennard-Jones force for a given distance.
+    """
+
     def __init__(self, R_CUT):
         self.__R_CUT = R_CUT
     # end __init__
 
     def LJ_potential(self, r):
+        """Calculate the Lennard-Jones potential for a given distance.
+
+        Parameters
+        ----------
+        r : float
+            Distance between particles.
+
+        Returns
+        -------
+        float
+            Lennard-Jones potential.
+        """
         r = float(r)
         if r < self.__R_CUT:
             V = 4.0 * ((1.0/r)**12.0 - (1.0/r)**6.0)
@@ -37,6 +113,18 @@ class Interaction:
     # end LJ_potential
     
     def LJ_force(self, r):
+        """Calculate the Lennard-Jones force for a given distance.
+
+        Parameters
+        ----------
+        r : float
+            Distance between particles.
+
+        Returns
+        -------
+        float
+            Lennard-Jones force.
+        """
         r = float(r)
         if r < self.__R_CUT:
             F = 4.0 * ((-12.0)*r**(-13.0) + 6.0*r**(-7.0))
@@ -48,12 +136,46 @@ class Interaction:
 # end Interaction
 
 class Box:
+    """Class representing a simulation box.
+
+    Parameters
+    ----------
+    BOX_SIDE_LENGTH : float
+        Side length of the simulation box.
+    R_CUT : float
+        Cutoff radius for interactions.
+
+    Attributes
+    ----------
+    _BOX_SIDE_LENGTH : float
+        Side length of the simulation box.
+    _interaction : Interaction
+        Interaction object for particle interactions.
+
+    Methods
+    -------
+    set_periodic_boundary(r)
+        Apply periodic boundary conditions to a distance.
+    """
+
     def __init__(self, BOX_SIDE_LENGTH, R_CUT):
         self._BOX_SIDE_LENGTH = BOX_SIDE_LENGTH
         self._interaction = Interaction(R_CUT) # using Composition for more flexible code ("has-a" relationship)
     # end def
 
     def set_periodic_boundary(self, r):
+        """Apply periodic boundary conditions to a distance.
+
+        Parameters
+        ----------
+        r : float
+            Distance to apply periodic boundary conditions.
+
+        Returns
+        -------
+        float
+            Distance with periodic boundary conditions applied.
+        """
         # Using minimum-image convention: only interact with the nearest copy of the particle (real or image)
         if (abs(r) > 0.5 * self._BOX_SIDE_LENGTH):
             r *= 1 - self._BOX_SIDE_LENGTH/abs(r)
@@ -62,6 +184,58 @@ class Box:
 # end Box    
 
 class Ensemble(Box): # Ensemble inherits from Box
+    """Class representing an ensemble of particles within a simulation box.
+
+    Parameters
+    ----------
+    BOX_SIDE_LENGTH : float
+        Side length of the simulation box.
+    PARTICLE_NUMBER : int
+        Number of particles in the ensemble.
+    R_CUT : float
+        Cutoff radius for interactions.
+
+    Attributes
+    ----------
+    _PARTICLE_NUMBER : int
+        Number of particles in the ensemble.
+    _particles : list of Particle
+        List containing Particle objects.
+    kinetic_energy : float
+        Total kinetic energy of the ensemble.
+    potential_energy : float
+        Total potential energy of the ensemble.
+    total_energy : float
+        Total energy of the ensemble.
+    temperature : float
+        Temperature of the ensemble.
+
+    Methods
+    -------
+    generate_particles()
+        Generate particles and set their initial positions.
+    set_initial_velocities(VELOCITY_AMPLITUDE)
+        Set initial velocities for all particles in the ensemble.
+    calculate_ensemble_momentum()
+        Calculate the total momentum of the ensemble.
+    calculate_ensemble_energy()
+        Calculate the total energy of the ensemble.
+    get_ensemble_temperature()
+        Calculate the temperature of the ensemble.
+    normalize_momentum()
+        Normalize the momentum of the ensemble.
+    update_ensemble_acceleration()
+        Update the acceleration of each particle in the ensemble.
+    propagate_velocity_verlet(dt)
+        Propagate particle positions and velocities using the Velocity-Verlet algorithm.
+    change_temperature(eta)
+        Change the temperature of the ensemble.
+    get_particle_positions()
+        Get the positions of all particles in the ensemble.
+    plot_ensemble()
+        Plot the ensemble configuration.
+    """
+
     def __init__(self, BOX_SIDE_LENGTH, PARTICLE_NUMBER, R_CUT): # initialize an empty box
         super().__init__(BOX_SIDE_LENGTH, R_CUT)
         self._PARTICLE_NUMBER = PARTICLE_NUMBER
@@ -69,6 +243,7 @@ class Ensemble(Box): # Ensemble inherits from Box
     # end __init__
 
     def generate_particles(self): # set the positions
+        """Generate particles and set their initial positions."""
         particle_root_1 = int(np.ceil(np.sqrt(self._PARTICLE_NUMBER)))          # 1st internal value to evenly place the particles
         particle_root_2 = int(np.ceil(self._PARTICLE_NUMBER / particle_root_1)) # 2nd internal value to evenly place the particles
         added_particles = 0
@@ -85,6 +260,13 @@ class Ensemble(Box): # Ensemble inherits from Box
     # end generate_particles
 
     def set_initial_velocities(self, VELOCITY_AMPLITUDE):
+        """Set initial velocities for all particles in the ensemble.
+
+        Parameters
+        ----------
+        VELOCITY_AMPLITUDE : float
+            Amplitude for generating initial velocities.
+        """
         for i in range(0, self._PARTICLE_NUMBER):
             Vx = VELOCITY_AMPLITUDE * (2 * np.random.rand() - 1)
             Vy = VELOCITY_AMPLITUDE * (2 * np.random.rand() - 1)
@@ -93,6 +275,13 @@ class Ensemble(Box): # Ensemble inherits from Box
     # end set_initial_velocities
 
     def calculate_ensemble_momentum(self):
+        """Calculate the total momentum of the ensemble.
+
+        Returns
+        -------
+        float
+            Total momentum of the ensemble.
+        """
         total_momentum = 0
         for i in range(0, self._PARTICLE_NUMBER):
             total_momentum += self._particles[i].get_momentum()
@@ -101,6 +290,13 @@ class Ensemble(Box): # Ensemble inherits from Box
     # end calculate_ensemble_momentum
 
     def calculate_ensemble_energy(self):
+        """Calculate the total energy of the ensemble.
+
+        Returns
+        -------
+        float
+            Total energy of the ensemble.
+        """
         total_kinetic = 0
         total_potential = 0
         for i in range(0, self._PARTICLE_NUMBER):
@@ -122,12 +318,20 @@ class Ensemble(Box): # Ensemble inherits from Box
     # end calculate_ensemble_energy
 
     def get_ensemble_temperature(self):
+        """Calculate the temperature of the ensemble.
+
+        Returns
+        -------
+        float
+            Temperature of the ensemble.
+        """
         # KE_avg = 3/2 * k * T
         self.temperature = 2/3 * self.kinetic_energy / self._PARTICLE_NUMBER
         return self.temperature
     # end get_ensemble_temperature
 
     def normalize_momentum(self):
+        """Normalize the momentum of the ensemble."""
         total_momentum = self.calculate_ensemble_momentum()
         for i in range(0, self._PARTICLE_NUMBER):
             delta_V = total_momentum / (self._particles[i]._MASS * self._PARTICLE_NUMBER)
@@ -136,6 +340,7 @@ class Ensemble(Box): # Ensemble inherits from Box
     # end normalize_momentum
 
     def update_ensemble_acceleration(self):
+        """Update the acceleration of each particle in the ensemble."""
         for i in range(0, self._PARTICLE_NUMBER):
             particle_acceleration = 0
             for j in range(0, self._PARTICLE_NUMBER):
@@ -150,6 +355,13 @@ class Ensemble(Box): # Ensemble inherits from Box
     # end update_ensemble_acceleration
 
     def propagate_velocity_verlet(self, dt): # this is the Velocity-Verlet algorithm
+        """Propagate particle positions and velocities using the Velocity-Verlet algorithm.
+
+        Parameters
+        ----------
+        dt : float
+            Time step for the integration.
+        """
         #self.update_ensemble_acceleration()
         # update the positions and partly velocity
         for i in range(0, self._PARTICLE_NUMBER):
@@ -164,12 +376,26 @@ class Ensemble(Box): # Ensemble inherits from Box
     # end propagate_velocity_verlet
 
     def change_temperature(self, eta):
+        """Change the temperature of the ensemble.
+
+        Parameters
+        ----------
+        eta : float
+            Scaling factor for velocities.
+        """
         for i in range(0, self._PARTICLE_NUMBER):
             self._particles[i].velocity = self._particles[i].velocity * eta
         # end for
     # end change_temperature
 
     def get_particle_positions(self):
+        """Get the positions of all particles in the ensemble.
+
+        Returns
+        -------
+        list of numpy.ndarray
+            List of particle positions.
+        """
         positions = []
         for i in range(0, self._PARTICLE_NUMBER):
             location = self._particles[i].location
@@ -179,6 +405,7 @@ class Ensemble(Box): # Ensemble inherits from Box
     # end get_particle_positions
 
     def plot_ensemble(self):
+        """Plot the ensemble configuration."""
         fig, ax = plt.subplots()
         ax.set_xlim(0, self._BOX_SIDE_LENGTH)
         ax.set_ylim(0, self._BOX_SIDE_LENGTH)
